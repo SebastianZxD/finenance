99c5d9451400f70ad4e304b5e06855b033712fc1
Sebastián Zambrano <juanzambrano97@hotmail.com>
2024-10-28 09:10:59 -0500

    feat: Created use-select-account hook for creating multiple transactions after the CSV import

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,23 +1,29 @@
 "use client";
 import { useState } from "react";
 
+import { transactions as transactionsSchema } from "@/db/schema";
+
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "@/components/data-table";
 import { 
   Card,
   CardContent,
   CardHeader,
   CardTitle, 
 } from "@/components/ui/card"
-import { DataTable } from "@/components/data-table";
 
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction"
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
+
+import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
 
 import { Loader2, Plus } from "lucide-react"
 
 import { columns } from "./columns";
-import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 import { UploadButton } from "./upload-button";
 import { ImportCard } from "./import-card";
 
@@ -33,6 +39,7 @@ const INITIAL_IMPORTS_RESULTS = {
 }
 
 const TransactionsPage = () => {
+  const [AccountDialog, confirm] = useSelectAccount();
   const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
   const [importResults, setImportResults] = useState(INITIAL_IMPORTS_RESULTS);
 
@@ -47,7 +54,7 @@ const TransactionsPage = () => {
   };
 
   const newTransaction = useNewTransaction();
-  console.log("Im here")
+  const createTransactions = useBulkCreateTransactions();
   const deleteTransactions = useBulkDeleteTransactions();
   const transactionsQuery = useGetTransactions();
   const transactions = transactionsQuery.data || [];
@@ -55,6 +62,26 @@ const TransactionsPage = () => {
   const isDisabled =
     transactionsQuery.isLoading ||
     deleteTransactions.isPending;
+
+  const onSubmitImport = async (
+    values: typeof transactionsSchema.$inferInsert[],
+  ) => {
+    const accountId = await confirm();
+    if (!accountId) {
+      return toast.error("Please select an account to continue.");
+    }
+
+    const data = values.map((value) => ({
+      ...value,
+      accountId: accountId as string,
+    }));
+
+    createTransactions.mutate(data, {
+      onSuccess: () => {
+        onCancelImport();
+      },
+    });
+  };
 
   if (transactionsQuery.isLoading) {
     return (
@@ -76,10 +103,11 @@ const TransactionsPage = () => {
   if (variant === VARIANTS.IMPORT) {
     return (
       <>
+        <AccountDialog />
         <ImportCard 
           data={importResults.data}
           onCancel={onCancelImport}
-          onSubmit={() => {}}
+          onSubmit={onSubmitImport}
         />
       </>
     )
