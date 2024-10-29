1a81cc074ce3d7ea7ac974fac45be482ceaa456a
Sebastián Zambrano <juanzambrano97@hotmail.com>
2024-10-29 12:10:42 -0500

    feat: Created summary API for dashboard data graphs

--- features/transactions/api/use-get-transaction.ts
+++ features/transactions/api/use-get-transaction.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
+import { convertAmountFromMiliUnits } from "@/lib/utils";
 
 export const useGetTransaction = (id?: string) => {
   const query = useQuery({
@@ -14,7 +15,10 @@ export const useGetTransaction = (id?: string) => {
       }
 
       const { data } = await response.json();
-      return data;
+      return {
+        ...data,
+        amount: convertAmountFromMiliUnits(data.amount),
+      }
     },
   });
 
